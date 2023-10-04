adb79c3a44eba16f1b1388e652e57fc1195140f4
root <root@DESKTOP-M9TD1RR.localdomain>
2023-10-04 15:03:23 +0200

    update 0-whatsmyname.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 
 /**
- * * main - prints the number of arguments
- * *@argc: number of arguments
- * *@argv:array of arguments
- * *Return: the number of arguments
- * */
+  * main - prints the number of arguments
+  *@argc: number of arguments
+  *@argv:array of arguments
+  *Return: the number of arguments
+  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
-		printf("%d\n", argc - 1);
+	int i;
 
-			return (0);
-}
-#include <stdio.h>
+	if (argc == 1)
+	        printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+				;
 
-/**
- * * main - prints the number of arguments
- * *@argc: number of arguments
- * *@argv:array of arguments
- * *Return: the number of arguments
- * */
-
-int main(int argc, char __attribute__((__unused__)) *argv[])
-{
-		printf("%d\n", argc - 1);
-
-			return (0);
+		printf("%d\n", i - 1);
+	}										
+	return (0);
 }
